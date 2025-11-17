c12c1f9b7e673c8c9e076468ae28f1f6b3b13288
Patrick <patrick.moore@wizards.com>
2025-11-17 10:33:26 -0500

    Update cert-install.sh

--- cert-install.sh
+++ cert-install.sh
@@ -18,9 +18,9 @@ docker run --rm \
   --dns 1.1.1.1 \
   --dns 1.0.0.1 \
   -e HOST_NAME="$HOST_NAME" \
-  -v ./cloudflare.ini:/cloudflare.ini:ro \
-  -v ./conf:/etc/letsencrypt \
-  -v ./logs:/var/log/letsencrypt \
+  -v /etc/certbot/cloudflare.ini:/cloudflare.ini:ro \
+  -v /var/log/certbot:/var/log/letsencrypt \
+  -v /opt/certbot/conf:/etc/letsencrypt \
   certbot/dns-cloudflare:latest \
   certonly -d "$HOST_NAME" \
   --dns-cloudflare \
@@ -30,15 +30,15 @@ docker run --rm \
   --agree-tos \
   --non-interactive
 
-if [ ! -f "conf/live/$HOST_NAME/privkey.pem" ]; then
-    echo "Error: No certbot certificate found in ./conf/live/$HOST_NAME/ to update $CERT_NAME"
+if [ ! -f "/opt/certbot/conf/live/$HOST_NAME/privkey.pem" ]; then
+    echo "Error: No certbot certificate found in /opt/certbot/conf/live/$HOST_NAME/ to update $CERT_NAME"
     exit 1
 fi
 
-if ! cmp -s "conf/live/$HOST_NAME/privkey.pem" "/usr/syno/etc/certificate/_archive/$CERT_NAME/privkey.pem"; then
+if ! cmp -s "/opt/certbot/conf/live/$HOST_NAME/privkey.pem" "/usr/syno/etc/certificate/_archive/$CERT_NAME/privkey.pem"; then
     # Copy certificate
     echo "New certbot certificate found... Copying to /usr/syno/etc/certificate/_archive/$CERT_NAME/"
-    cp conf/live/$HOST_NAME/* /usr/syno/etc/certificate/_archive/$CERT_NAME/
+    cp /opt/certbot/conf/live/$HOST_NAME/* /usr/syno/etc/certificate/_archive/$CERT_NAME/
 
     # Restart synology services
     echo -n "Restarting..."
